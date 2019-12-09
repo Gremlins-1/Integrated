5ae1631a85a4175a54d17f999f21d9f3aff88314
Noland Seigler <nolandseigler@gmail.com>
2019-12-08 23:24:49 -0600

    redid it all because i could not get it working again. OAUTH REDOES THE TOKENS EACH TIME YOU REINSTALL THE APP. I WAS TESTING WITH THE WRONG TOKEN. bot now says hello world and greets the user by username on the first entry to app home. each subsequent entry causes a secondary greeting. glitch hosting is workin great.

--- index.js
+++ index.js
@@ -1,78 +1,33 @@
-// Require the Bolt package (github.com/slackapi/bolt)
-const { App: Index } = require("@slack/bolt");
+const { App } = require('@slack/bolt');
+const store = require('./store');
 
-const app = new Index({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN
 });
 
 
+app.event('app_home_opened', ({ event, say }) => {
+    // Look up the user from DB
+    let user = store.getUser(event.user);
 
-// All the room in the world for your code
+    if(!user) {
+        user = {
+            user: event.user,
+            channel: event.channel
+        };
+        store.addUser(user);
+
+        say(`Hello world, and welcome <@${event.user}>! Atlas is under construction. If you have any feedback or feature suggestions please reach us at totallyanemail@ourdomain.com`);
+    } else {
+        say('Welcome back! How may I assist you?');
+    }
+});
 
 
-
+// Start your app
 (async () => {
-    // Start your app
     await app.start(process.env.PORT || 3000);
-
     console.log('⚡️ Bolt app is running!');
 })();
-//Start of thing for the app home. Not sure if it works. Hosting this thing seems to be an issue. Came from the glitch page? All done by the guide.
-// app.event('app_home_opened', async ({ event, context }) => {
-//     try {
-//         /* view.publish is the method that your app uses to push a view to the Home tab */
-//         const result = await app.client.views.publish({
-//
-//             /* retrieves your xoxb token from context */
-//             token: context.botToken,
-//
-//             /* the user that opened your app's app home */
-//             user_id: event.user,
-//
-//             /* the view payload that appears in the app home*/
-//             view: {
-//                 type: 'home',
-//                 callback_id: 'home_view',
-//
-//                 /* body of the view */
-//                 blocks: [
-//                     {
-//                         "type": "section",
-//                         "text": {
-//                             "type": "mrkdwn",
-//                             "text": "*Welcome to your _App's Home_* :tada:"
-//                         }
-//                     },
-//                     {
-//                         "type": "divider"
-//                     },
-//                     {
-//                         "type": "section",
-//                         "text": {
-//                             "type": "mrkdwn",
-//                             "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-//                         }
-//                     },
-//                     {
-//                         "type": "actions",
-//                         "elements": [
-//                             {
-//                                 "type": "button",
-//                                 "text": {
-//                                     "type": "plain_text",
-//                                     "text": "Click me!"
-//                                 }
-//                             }
-//                         ]
-//                     }
-//                 ]
-//             }
-//         });
-//     }
-//     catch (error) {
-//         console.error(error);
-//     }
-// });
-// End of the event listener for the home app stuff
-// Commenting it out changes nothing...so it is broken
+

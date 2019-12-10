2dad777ea06dc04344f3f44dc2f34282a7ae4b0f
Noland Seigler <nolandseigler@gmail.com>
2019-12-10 16:44:44 -0600

    updated the bot with a squirrel emoji adder, and random music link generator. all commands now require a prefix of %

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ app.event('app_home_opened', ({ event, say }) => {
     console.log('⚡️ Bolt app is running!');
 })();
 //THIS WORKSSSS!!!!!!!
-app.message('Who was Romulus?', ({ say }) => say("Romulus was the legendary founder and first king of Rome. Various traditions attribute the establishment of many of Rome's oldest legal, political, religious, and social institutions to Romulus and his contemporaries."));
+app.message('%whoWasRomulus', ({ say }) => say("Romulus was the legendary founder and first king of Rome. Various traditions attribute the establishment of many of Rome's oldest legal, political, religious, and social institutions to Romulus and his contemporaries."));
 //THIS WORKKKKSS!!!!!!!^
-
+//THIS WORKSSSS!!!!!!!
 app.message(/open the (.*) doors/i, ({ say, context }) => {
     const doorType = context.matches[1];
 
@@ -46,15 +46,12 @@ app.message(/open the (.*) doors/i, ({ say, context }) => {
     say(text);
 });
 
-// robot.hear(/I like pie/i, (res) => {
-//   res.emote('makes a freshly baked pie')
-// })
-
-app.message('I like pie', async ({ message, context }) => {
+//THIS WORKSSSS!!!!!!!
+app.message('%squirrelThis', async ({ message, context }) => {
     try {
         await app.client.reactions.add({
             token: context.botToken,
-            name: 'pie',
+            name: 'squirrel',
             channel: message.channel,
             timestamp: message.ts,
         });
@@ -62,183 +59,22 @@ app.message('I like pie', async ({ message, context }) => {
         console.error(error);
     }
 });
+//THIS WORKKKKSS!!!!!!!^
 //THIS WORKSSSS!!!!!!!
-const lulz = ['lol', 'rofl', 'lmao'];
+const music = ["https://www.youtube.com/watch?v=2TvWZEVf6go", "https://www.youtube.com/watch?v=y7e-GC6oGhg", "https://www.youtube.com/watch?v=8c9yOlPVpak", "https://www.youtube.com/watch?v=TDcJJYY5sms"];
 
-// robot.respond(`/${lulz.join('|')}/i`, (res) => {
-//   res.send(res.random(lulz))
-// })
-
-const randomLulz = () => lulz[Math.floor(Math.random() * lulz.length)];
-
-app.event('app_mention', ({ say }) => say(randomLulz()));
+const randomMusic = () => music[Math.floor(Math.random() * music.length)];
+//THIS IS HOW YOU GET THE RESULT OF THE FUNCTION TO BE POSTED
+app.message('%music', ({ say }) => say(randomMusic()));
+// app.event('app_mention', ({ say }) => say(randomMusic()));
 //THIS WORKKKKSS!!!!!!!^
 
-const enterReplies = ['Hi', 'Target Acquired', 'Firing', 'Hello friend.', 'Gotcha', 'I see you']
-const leaveReplies = ['Are you still there?', 'Target lost', 'Searching']
+const enterReplies = ["Welcome!", "Howdy!", "Hello friend.", "Gotcha", "Hello there.", "I see you"]
+const leaveReplies = ['Goodbye', 'Adios', 'Uh oh']
 
-// robot.enter((res) => {
-//   res.send(res.random(enterReplies))
-// })
-// robot.leave((res) => {
-//   res.send(res.random(leaveReplies))
-// })
 
 const randomEnterReply = () => enterReplies[Math.floor(Math.random() * enterReplies.length)];
 const randomLeaveReply = () => leaveReplies[Math.floor(Math.random() * leaveReplies.length)];
 
 app.event('member_joined_channel', ({ say }) => say(randomEnterReply()));
 app.event('member_left_channel', ({ say }) => say(randomLeaveReply()));
-
-const answer = process.env.HUBOT_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING
-
-// robot.respond(/what is the answer to the ultimate question of life/, (res) => {
-//   if (answer) {
-//     res.send(`${answer}, but what is the question?`)
-//     return
-//   }
-//
-//   res.send('Missing HUBOT_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING in environment: please set and try again')
-// })
-
-// app.message(
-//   directMention(),
-//   'what is the answer to the ultimate question of life',
-//   ({ say }) => {
-//     if (answer) { say(`${answer}, but what is the question?`); }
-//   });
-
-//   // robot.respond(/you are a little slow/, (res) => {
-//   //   setTimeout(() => res.send('Who you calling "slow"?'), 60 * 1000)
-//   // })
-
-// app.message('you are a little slow', ({ say }) => {
-//   setTimeout(() => say('Who you calling "slow"?'), 60 * 1000);
-// });
-
-// let annoyIntervalId = null
-
-//   // robot.respond(/annoy me/, (res) => {
-//   //   if (annoyIntervalId) {
-//   //     res.send('AAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIHHHHHHHHHH')
-//   //     return
-//   //   }
-//   //
-//   //   res.send('Hey, want to hear the most annoying sound in the world?')
-//   //   annoyIntervalId = setInterval(() => res.send('AAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIHHHHHHHHHH'), 1000)
-//   // })
-//   //
-//   // robot.respond(/unannoy me/, (res) => {
-//   //   if (!annoyIntervalId) {
-//   //     res.send('Not annoying you right now, am I?')
-//   //     return
-//   //   }
-//   //
-//   //   res.send('OKAY, OKAY, OKAY!')
-//   //   clearInterval(annoyIntervalId)
-//   //   annoyIntervalId = null
-//   // })
-
-// app.message(directMention(), /(?<!un)annoy me/, ({ say }) => {
-//   if (annoyIntervalId) {
-//     say('AAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIHHHHHHHHHH');
-//     return;
-//   }
-
-//   say('Hey, want to hear the most annoying sound in the world?');
-//   annoyIntervalId = setInterval(() => {
-//     say('AAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIHHHHHHHHHH');
-//   }, 1000);
-// });
-
-// app.message(directMention(), 'unannoy me', ({ say }) => {
-//   if (!annoyIntervalId) {
-//     say('Not annoying you right now, am I?');
-//     return;
-//   }
-//   say('OKAY, OKAY, OKAY!');
-//   clearInterval(annoyIntervalId);
-//   annoyIntervalId = null;
-// });
-
-//   // robot.router.post('/hubot/chatsecrets/:room', (req, res) => {
-//   //   const room = req.params.room
-//   //   const data = JSON.parse(req.body.payload)
-//   //   const secret = data.secret
-//   //
-//   //   robot.messageRoom(room, `I have a secret: ${secret}`)
-//   //
-//   //   res.send('OK')
-//   // })
-
-//   // 🚫 stand up your own express router
-
-//   // robot.error((error, response) => {
-//   //   const message = `DOES NOT COMPUTE: ${error.toString()}`
-//   //   robot.logger.error(message)
-//   //
-//   //   if (response) {
-//   //     response.reply(message)
-//   //   }
-//   // })
-
-// app.error((error) => {
-//   const message = `DOES NOT COMPUTE: ${error.toString()}`;
-//   console.error(message);
-
-//   // 🚫 no reply handling from global error handler
-// });
-
-//   // robot.respond(/have a soda/i, (response) => {
-//   //   // Get number of sodas had (coerced to a number).
-//   //   const sodasHad = +robot.brain.get('totalSodas') || 0
-//   //
-//   //   if (sodasHad > 4) {
-//   //     response.reply('I’m too fizzy…')
-//   //     return
-//   //   }
-//   //
-//   //   response.reply('Sure!')
-//   //   robot.brain.set('totalSodas', sodasHad + 1)
-//   // })
-//   //
-//   // robot.respond(/sleep it off/i, (res) => {
-//   //   robot.brain.set('totalSodas', 0)
-//   //   res.reply('zzzzz')
-//   // })
-
-// // NOTE: In a real application, you should provide a convoStore option to the App constructor. The default convoStore
-// //       only persists data to memory, so its lost when the process terminates.
-// app.message(directMention(), 'have a soda', async ({ context, say }) => {
-//   // Initialize conversation
-//   const conversation = context.conversation !== undefined ? context.conversation : {};
-
-//   // Initialize data for this listener
-//   conversation.sodasHad = conversation.sodasHad !== undefined ? conversation.sodasHad : 0;
-
-//   if (conversation.sodasHad > 4) {
-//     say('I\'m too fizzy...');
-//     return;
-//   }
-
-//   say('Sure!');
-//   conversation.sodasHad += 1;
-//   try {
-//     await context.updateConversation(conversation);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
-// app.message(directMention(), 'sleep it off', async ({ context, say }) => {
-//   try {
-//     await context.updateConversation({ ...context.conversation, sodasHad: 0 });
-//     say('zzzzz');
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
-
-
-
-

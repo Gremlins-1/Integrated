89bc018150996ac38d1cc2414302c0a160c6f631
Noland Seigler <nolandseigler@gmail.com>
2019-12-11 19:04:08 -0600

    minor modifications. mainly added comments for clarity. updating glitch with this and then working on the express ngrok stuff. if you are reading this dm me please.

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-// First we need to import the HTTP module. This module contains all the logic for dealing with HTTP requests.
+// import http module
 var http = require('http');
 
-// We define the port we want to listen to. Logically this has to be the same port than we specified on ngrok.
+// ngrok listener port. consider updating this to 443 for TLS?
 const PORT=1229;
 
-// We create a function which handles any requests and sends a simple response
+// requests come in and response.end goes out. this works with slash commands but for it to work with Events API i need to get it to send back the proper challenge.
 function handleRequest(request, response){
     response.end('Ngrok is working! -  Path Hit: ' + request.url);
 }
 
-// We create the web server object calling the createServer function. Passing our request function onto createServer guarantees the function is called once for every HTTP request that's made against the server
+//serve is created by passing handleRequest look above...into the createServer method. Function is called with every HTTP request.
 var server = http.createServer(handleRequest);
 
-// Finally we start the server
+// start server?
 server.listen(PORT, function(){
-    // Callback triggered when server is successfully listening. Hurray!
+    // Callback triggered when server is successfully listening.?
     console.log("Server listening on: http://localhost:%s", PORT);
 });
+//import express and request
 var express = require('express');
 var request = require('request');
-// Instantiates Express and assigns our app variable to it
-var app = express();
+// instantiates express and assigns it to the const app was originally var
+const app = express();
 
 
 // Again, we define a port we want to listen to
-// const PORT=4390;
 
-// Lets start our server
+
+// start app?
 app.listen(PORT, function () {
-    //Callback triggered when server is successfully listening. Hurray!
+    //Callback triggered when app is successfully listening?
     console.log("Example app listening on port " + PORT);
 });
 
 
-// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+// This route handles GET requests to our root (NOTICE THE '/') ngrok address and responds with the same "Ngrok is working message" we used before. MAYBE THIS IS THE ONE FOR CHALLENGE.
 app.get('/', function(req, res) {
     res.send('Ngrok is working! Path Hit: ' + req.url);
 });
 
 // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+//OAUTH IS SUPER GREAT BUT IS NOT A CONCERN AT THIS PHASE.
 app.get('/oauth', function(req, res) {
     // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
     if (!req.query.code) {
@@ -72,84 +74,98 @@ app.post('/command', function(req, res) {
 
 
 
-//above this is stuff for hosting through ngrok
-// const { App } = require('@slack/bolt');
-// const store = require('./store');
-//
-// const app = new App({
-//     signingSecret: process.env.SLACK_SIGNING_SECRET,
-//     token: process.env.SLACK_BOT_TOKEN
-// });
+// above this is the express server for /commands it is hosted on my machine using ngrok
 //
 //
 //
-// app.event('app_home_opened', ({ event, say }) => {
-//     // Look up the user from DB
-//     let user = store.getUser(event.user);
 //
-//     if(!user) {
-//         user = {
-//             user: event.user,
-//             channel: event.channel
-//         };
-//         store.addUser(user);
-//
-//         say(`Hello <@${event.user}>! I am Romulus. I am currently in development.`);
-//     } else {
-//         say(`Today is I can't get date to work.`);
-//     }
-// });
+// below this line is turned on for events and is used inside glitch rn
 //
 //
 //
-// // Start your app
-// (async () => {
-//     await app.start(process.env.PORT || 3000);
-//     console.log('⚡️ Bolt app is running!');
-// })();
-// //THIS WORKSSSS!!!!!!!
-// app.message('%whoWasRomulus', ({ say }) => say("Romulus was the legendary founder and first king of Rome. Various traditions attribute the establishment of many of Rome's oldest legal, political, religious, and social institutions to Romulus and his contemporaries."));
-// //THIS WORKKKKSS!!!!!!!^
-// //THIS WORKSSSS!!!!!!!
-// app.message(/open the (.*) doors/i, ({ say, context }) => {
-//     const doorType = context.matches[1];
-//
-//     const text = (doorType === 'pod bay') ?
-//         'I’m afraid I can’t let you do that.' :
-//         `Opening ${doorType} doors`;
-//
-//     say(text);
-// });
-//
-// //THIS WORKSSSS!!!!!!!
-// app.message('%squirrelThis', async ({ message, context }) => {
-//     try {
-//         await app.client.reactions.add({
-//             token: context.botToken,
-//             name: 'squirrel',
-//             channel: message.channel,
-//             timestamp: message.ts,
-//         });
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
-// //THIS WORKKKKSS!!!!!!!^
-// //THIS WORKSSSS!!!!!!!
-// const music = ["https://www.youtube.com/watch?v=2TvWZEVf6go", "https://www.youtube.com/watch?v=y7e-GC6oGhg", "https://www.youtube.com/watch?v=8c9yOlPVpak", "https://www.youtube.com/watch?v=TDcJJYY5sms"];
-//
-// const randomMusic = () => music[Math.floor(Math.random() * music.length)];
-// //THIS IS HOW YOU GET THE RESULT OF THE FUNCTION TO BE POSTED
-// app.message('%music', ({ say }) => say(randomMusic()));
-// // app.event('app_mention', ({ say }) => say(randomMusic()));
-// //THIS WORKKKKSS!!!!!!!^
-//
-// const enterReplies = ["Welcome!", "Howdy!", "Hello friend.", "Gotcha", "Hello there.", "I see you"]
-// const leaveReplies = ['Goodbye', 'Adios', 'Uh oh']
+// start the party by creating the App CLASS? and requiring all the awesome slack API's. they are part of the bolt framework. refer to package-lock if interested.
+const { App } = require('@slack/bolt');
+//require our store that we have not made yet.
+const store = require('./store');
+//instantiate the object app as a member of the App class. pass in an object with our private key for signing messages and our OAUTH bot token. we are using the environment variables to do this so we dont blast our keys/tokens to the world. Be sure to put .env in the .gitignore
+const app = new App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN
+});
+
+
 //
 //
-// const randomEnterReply = () => enterReplies[Math.floor(Math.random() * enterReplies.length)];
-// const randomLeaveReply = () => leaveReplies[Math.floor(Math.random() * leaveReplies.length)];
 //
-// app.event('member_joined_channel', ({ say }) => say(randomEnterReply()));
-// app.event('member_left_channel', ({ say }) => say(randomLeaveReply()));
+//
+//uses the event api listener to trigger on the app_home_opened eventName. the function takes in the event then calls the function.
+app.event('app_home_opened', ({ event, say }) => {
+    // Look up the user from DB. This will work one day but today is not that day. An interesting note is that there seems to be some cache. I have not timed it but for a few mins/hours the system believes user is true. (click the app a bunch and watch). the app home functionality is not turned on and it is in beta. what is funny is that this still works for dms to the app.
+    let user = store.getUser(event.user);
+
+    if(!user) {
+        user = {
+            user: event.user,
+            channel: event.channel
+        };
+        store.addUser(user);
+
+        say(`Hello <@${event.user}>! I am Romulus. I am currently in development.`);
+    } else {
+        say(`Today is I can't get date to work.`);
+    }
+});
+
+
+
+// Start your app with async. Based on the watching a youtube vid from JSConf this allows the app to start but makes it wait outside of the stack within the web api, once the async timer expires it then goes to the queue where it waits for the stack to be clear before executing.
+(async () => {
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running!');
+})();
+//THIS WORKSSSS!!!!!!!
+app.message('%whoWasRomulus', ({ say }) => say("Romulus was the legendary founder and first king of Rome. Various traditions attribute the establishment of many of Rome's oldest legal, political, religious, and social institutions to Romulus and his contemporaries."));
+//THIS WORKKKKSS!!!!!!!^
+//THIS WORKSSSS!!!!!!!
+app.message(/this is the (.*)/i, ({ say, context }) => {
+    const mando = context.matches[2];
+//make this send a picture of mando if correct.
+    const text = (mando === 'way') ?
+        'This is the way.' :
+        `We do not take Imperial Credits here`;
+
+    say(text);
+});
+
+//THIS WORKSSSS!!!!!!!
+app.message('%squirrelThis', async ({ message, context }) => {
+    try {
+        await app.client.reactions.add({
+            token: context.botToken,
+            name: 'squirrel',
+            channel: message.channel,
+            timestamp: message.ts,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
+//THIS WORKKKKSS!!!!!!!^
+//THIS WORKSSSS!!!!!!!
+const music = ["https://www.youtube.com/watch?v=2TvWZEVf6go", "https://www.youtube.com/watch?v=y7e-GC6oGhg", "https://www.youtube.com/watch?v=8c9yOlPVpak", "https://www.youtube.com/watch?v=TDcJJYY5sms"];
+
+const randomMusic = () => music[Math.floor(Math.random() * music.length)];
+//THIS IS HOW YOU GET THE RESULT OF THE FUNCTION TO BE POSTED
+app.message('%music', ({ say }) => say(randomMusic()));
+// app.event('app_mention', ({ say }) => say(randomMusic()));
+//THIS WORKKKKSS!!!!!!!^
+
+const enterReplies = ["Welcome!", "Howdy!", "Hello friend.", "Gotcha", "Hello there.", "I see you"]
+const leaveReplies = ['Goodbye', 'Adios', 'Uh oh']
+
+
+const randomEnterReply = () => enterReplies[Math.floor(Math.random() * enterReplies.length)];
+const randomLeaveReply = () => leaveReplies[Math.floor(Math.random() * leaveReplies.length)];
+
+app.event('member_joined_channel', ({ say }) => say(randomEnterReply()));
+app.event('member_left_channel', ({ say }) => say(randomLeaveReply()));

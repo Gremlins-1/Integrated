6cb1b073a0f6efa9d95d1d09804961604b016cc1
Noland Seigler <nolandseigler@gmail.com>
2020-01-05 23:03:20 -0600

    new functionality to be tested and hosted on a dif glitch page than main

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const qs = require('qs');
+const signature = require('./verify-signature');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const rawBodyBuffer = (req, res, buf, encoding) => {
+    if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || 'utf8');
+    }
+};
+
+app.use(bodyParser.urlencoded({verify: rawBodyBuffer, extended: true }));
+app.use(bodyParser.json({ verify: rawBodyBuffer }));
 
 const server = app.listen(5000); //port to listen on
 
@@ -55,7 +62,8 @@ app.post('actions', (req, res) => {
             })
         };
         //open the dialog by calling dialogs.open methdd and sending the payload
-        axios.post('https://slack.com/api/dialog.open', qs.stringify(dialogData)).then((result) => {
+        axios.post('https://slack.com/api/dialog.open', qs.stringify(dialogData))
+            .then((result) => {
                 if(result.data.error) {
                     res.sendStatus(500);
                 } else {
@@ -66,6 +74,40 @@ app.post('actions', (req, res) => {
                 res.sendStatus(500);
             });
     } else if (type === 'dialog_submission') {
+        res.send('');
 
+        //save the data in DB. Future functionality?
+        db.set(user.id, submission);
+        //DM the user a confirmation message
+        const attachments = [
+            {
+                title: 'Saved It!',
+                title_link: `http://example.com/${user.id}/clip`,
+                fields: [
+                    {
+                        title: 'Message',
+                        value: submission.message
+                    },
+                    {
+                        title: 'Importance',
+                        value: submission.importance,
+                        short: true
+                    },
+                ],
+            },
+        ];
+        const message = {
+            token: process.env.SLACK_BOT_TOKEN,
+            channel: user.id,
+            as_user: true, //bot user
+            attachments: JSON.stringify(attachments);
+        };
+        axios.post('https://slack.com/api/chat.postMessage', qs.stringify(message))
+            .then((result => {
+                console.log(result.data);
+            }))
+            .catch((err) => {
+                console.log(err);
+            })
     }
 });
